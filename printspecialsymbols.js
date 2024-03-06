fce11372183bd838c8f1158e921e3d34dee9c75f
유원 박 <ydbdnjs@outlook.kr>
2024-03-07 07:15:39 +0900

    day1

---
+++ printspecialsymbols.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('close', function () {
+    console.log(`!@#$%^&*(\\'"<>?:;`);
+});
